20ba644b57066d087d1683f90f9b31b395a6b579
coderDaiyan <daiyanabdallah@gmail.com>
2021-04-15 04:16:32 +0600

    service data loaded from mongo

--- src/Components/Home/Services/Services.js
+++ src/Components/Home/Services/Services.js
@@ -1,54 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ServiceCard from "../ServiceCard/ServiceCard";
 import "./Services.css";
 
 const Services = () => {
-  const servicesData = [
-    {
-      icon: "https://i.ibb.co/5FQvQCX/wheel.png",
-      title: "Break Repair",
-      description:
-        "Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne vim enim eu, his rebum iriure eripuit graecis et.eripuit graecis et.",
-    },
-    {
-      icon: "https://i.ibb.co/FKJjc0M/oil-tank.png",
-      title: "Oil & Filters",
-      description:
-        "Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne vim enim eu, his rebum iriure eripuit graecis et.eripuit graecis et.",
-    },
-    {
-      icon: "https://i.ibb.co/gmh7Dw7/ignition.png",
-      title: "Ignition Test",
-      description:
-        "Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne vim enim eu, his rebum iriure eripuit graecis et.eripuit graecis et.",
-    },
-    {
-      icon: "https://i.ibb.co/8P6XJ9W/freezer.png",
-      title: "A/C Recharge",
-      description:
-        "Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne vim enim eu, his rebum iriure eripuit graecis et.eripuit graecis et.",
-    },
-  ];
+  const [services, setServices] = useState([]);
 
-  const addServicehandle = () => {
-    fetch("http://localhost:5000/addService", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(servicesData),
-    });
-  };
+  useEffect(() => {
+    fetch("http://localhost:5000/services")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
 
   return (
     <section className="mt-5">
       <h1 className="text-center">Our Services</h1>
-      <button onClick={addServicehandle} className="btn">
-        Add Service
-      </button>
       <div
         style={{ width: "99vw" }}
         className="row mt-4 d-flex justify-content-center align-items-center"
       >
-        {servicesData.map((service) => (
+        {services.map((service) => (
           <ServiceCard service={service} />
         ))}
       </div>
